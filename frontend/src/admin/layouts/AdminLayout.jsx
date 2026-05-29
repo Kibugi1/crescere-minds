f34811f22b7844dc74ece33141cf81c87b7ca811
Kibugi1 <njugunabrian843@gmail.com>
2026-05-29 20:42:28 +0300

    fix: replace DashboardHome component with Outlet for routing

--- frontend/src/admin/layouts/AdminLayout.jsx
+++ frontend/src/admin/layouts/AdminLayout.jsx
@@ -83,7 +83,7 @@ export default function AdminLayout() {
                     }}
                 >
 
-                    <DashboardHome />
+                    <Outlet />
 
                 </Box>
 
