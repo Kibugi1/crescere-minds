21e0f709d64f89b2e75944bb03ddbcd2f8248466
Kibugi1 <njugunabrian843@gmail.com>
2026-05-29 20:18:53 +0300

    feat: add Vercel configuration for URL rewrites

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -44,7 +44,6 @@ export default function AppRoutes() {
                         path="dashboard"
                         element={<DashboardHome />}
                     />
-
                 </Route>
 
             </Routes>
