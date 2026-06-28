6137265eacf55e344993265dac9d2149e36314b3
Kibugi1 <njugunabrian843@gmail.com>
2026-06-28 19:48:27 +0300

    fix: update image URL path in SingleBlog component for correct blog image display

--- frontend/src/pages/SingleBlog.jsx
+++ frontend/src/pages/SingleBlog.jsx
@@ -175,7 +175,7 @@ export default function SingleBlog() {
 
                     src={
                         blog.image
-                            ? `http://127.0.0.1:5000/api/blogs/uploads/${blog.image}`
+                            ? `http://127.0.0.1:5000/api/uploads/${blog.image}`
                             : ""
                     }
 
